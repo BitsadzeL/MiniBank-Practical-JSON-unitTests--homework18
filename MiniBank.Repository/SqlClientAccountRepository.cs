d7956c344abdc1dfe82523743a541271d2ac38cd
BitsadzeL <bitsadzeluka@gmail.com>
2025-01-23 04:15:56 +0400

    customer and account reposiroty using sql client and tests for them.

---
+++ MiniBank.Repository/SqlClientAccountRepository.cs
@@ -0,0 +1,139 @@
+﻿using MiniBank.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MiniBank.Repository
+{
+    public class SqlClientAccountRepository
+    {
+        private const string _connectionString = "Server=DESKTOP-VU8IK37\\SQLEXPRESS;Database=MiniBank;Trusted_Connection=true;TrustServerCertificate=true";
+
+        public async Task<List<Account>> GetAccounts()
+        {
+            List<Account> accounts = new List<Account>();
+
+
+            using (SqlConnection connection = new (_connectionString)) 
+            {
+                using (SqlCommand command = new ("spGetAllAccounts",connection)) 
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    
+                    await connection.OpenAsync();
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (await reader.ReadAsync())
+                    {
+                        Account account = new Account();
+                        account.Id = reader.GetInt32(0);
+                        account.Iban= reader.GetString(1);
+                        account.Currency= reader.GetString(2);
+                        account.Balance=reader.GetDecimal(3);
+                        account.Name= reader.GetString(4);
+                        account.CustomerId = reader.GetInt32(5);
+
+                        accounts.Add(account);
+
+                    }
+                }
+            }
+
+            return accounts;
+        }
+
+        public async Task<List<Account>> GetUserAccount(int Id) 
+        {
+            List<Account> userAccounts = new();
+            using (SqlConnection connection = new(_connectionString))
+            {
+                using (SqlCommand command = new("spGetUserAccount", connection))
+                {
+                    command.CommandType=CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("customerId", Id);
+
+                    await connection.OpenAsync();
+                    SqlDataReader reader=await command.ExecuteReaderAsync();
+
+                    while(await reader.ReadAsync()) 
+                    {
+
+                        Account account = new Account();
+                        account.Id = reader.GetInt32(0);
+                        account.Iban = reader.GetString(1);
+                        account.Currency = reader.GetString(2);
+                        account.Balance = reader.GetDecimal(3);
+                        account.Name = reader.GetString(4);
+                        account.CustomerId = reader.GetInt32(5);
+
+                        userAccounts.Add(account);
+
+                    }
+
+                }
+            }
+
+            return userAccounts;
+            
+        }
+
+        public async Task CreateAccount(Account acc)
+        {
+            using (SqlConnection connection = new(_connectionString))
+            {
+                using (SqlCommand command = new("spAddNewAccount", connection))
+                {
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("iban", acc.Iban);
+                    command.Parameters.AddWithValue("currency", acc.Currency);
+                    command.Parameters.AddWithValue("balance", acc.Balance);
+                    command.Parameters.AddWithValue("name", acc.Name);
+                    command.Parameters.AddWithValue("customerId", acc.CustomerId);
+
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+
+
+
+                }
+            }
+        }
+
+        public async Task DeleteAccount(int Id)
+        {
+            using(SqlConnection connection = new(_connectionString))
+            {
+                using (SqlCommand command = new("spDeleteAccount", connection))
+                {
+                    command.CommandType=CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("accountId", Id);
+
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task UpdateAccount(Account acc)
+        {
+            using(SqlConnection connection = new(_connectionString))
+            {
+                using (SqlCommand command = new("spUpdateAccount", connection))
+                {
+                    command.CommandType =CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("accountId",acc.Id);
+                    command.Parameters.AddWithValue("iban", acc.Iban);
+                    command.Parameters.AddWithValue("currency", acc.Currency);
+                    command.Parameters.AddWithValue("balance", acc.Balance);
+                    command.Parameters.AddWithValue("name",acc.Name);
+                    command.Parameters.AddWithValue("customerId",acc.CustomerId);
+
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+
+                }
+            }
+        }
+    }
+}
---
+++ MiniBank.Tests/Sql_Account_Repository_Should.cs
@@ -0,0 +1,72 @@
+﻿using MiniBank.Models;
+using MiniBank.Repository;
+
+namespace MiniBank.Tests
+{
+    public class Sql_Account_Repository_Should
+    {
+        
+        SqlClientAccountRepository _sqlClientAccountRepository;
+        public Sql_Account_Repository_Should()
+        {
+            _sqlClientAccountRepository = new();
+        }
+
+
+
+        [Fact]
+        public async Task Add_Account()
+        {
+            Account acc= new()
+            {
+                Iban = "GE74SB7041487456391153",
+                Currency = "USD",
+                Balance = 10000,
+                Name = "",
+                CustomerId=2
+            };
+
+            await _sqlClientAccountRepository.CreateAccount(acc);
+        }
+
+
+
+        [Fact]
+        public async Task Get_All_Accounts()
+        {
+            var result = await _sqlClientAccountRepository.GetAccounts();
+        }
+
+        [Fact]
+        public async Task Get_All_Accounts_Of_User()
+        {
+            var result = await _sqlClientAccountRepository.GetUserAccount(2);
+        }
+
+
+
+        [Fact]
+        public async Task Delete_Account()
+        {
+            await _sqlClientAccountRepository.DeleteAccount(1);
+        }
+
+
+        [Fact]
+        public async Task Update_Account()
+        {
+            Account acc = new()
+            {
+                Id = 3,
+                Iban = "GE74SB7041487456391153",
+                Currency = "USD",
+                Balance = 1250.25m,
+                Name = "my car",
+                CustomerId = 2
+            };
+
+            await _sqlClientAccountRepository.UpdateAccount(acc);
+        }
+
+    }
+}
